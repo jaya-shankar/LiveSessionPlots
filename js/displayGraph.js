ae40e061eccdfdc4d6f45a8f3492afa3abcedd7c
jaya-shankar <bjayashankar2102@gmail.com>
2022-07-04 22:10:30 +0200

    "added graphes"

--- js/displayGraph.js
+++ js/displayGraph.js
@@ -1,30 +1,10 @@
-
-
-var file_name = ["graphs/20-24-Higher_Secondary_fin.png",
-                "graphs/20-24-Lower_Secondary_fin.png",
-                "graphs/gdp_per_capita (1).png",
-                "graphs/gdp_per_capita (1).png"]
-
-// const jsonData= require('./plotNames.json'); 
-// console.log(jsonData);
-
+var file_names = ['8.png', '4.png', '5.png', '7.png', '6.png', '2.png', '3.png', '1.png'];
 document.addEventListener('DOMContentLoaded', function() {
 
-    fetch('./plotNames.json')
-  .then(response => response.json())
-  .then(data => console.log(data))
-  .catch(error => console.log(error));
-
-    
-    // console.log(file_name);
-    // for each file name in the array
-    for (var i = 0; i < file_name.length; i++) {
-        // create a new image element
+    for (var i = 0; i < file_names.length; i++) {
         var img = document.createElement("img");
-        // set the src attribute of the image to the current file name
-        img.src = file_name[i];
+        img.src = "graphs/"+file_names[i];
         img.className = 'centerImg'
-        // append the image to the body
         document.body.appendChild(img);
     }
 
