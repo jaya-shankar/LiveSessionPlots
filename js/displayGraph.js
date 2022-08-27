8ad44990b2ffd9e8d02dbc1918e116c77e40216b
jaya-shankar <bjayashankar2102@gmail.com>
2022-08-27 16:56:44 +0200

    "added graphes"

--- js/displayGraph.js
+++ js/displayGraph.js
@@ -1,10 +1,17 @@
 var file_names = ['1.png', '2.png', '3.png', '5.png', '6.png', '7.png', '8.png'];
 document.addEventListener('DOMContentLoaded', function() {
     let url = window.location.href;
-    let folder_name = url.substring(url.lastIndexOf('?') + 1);
-    folder_name = parseInt(folder_name.substring(folder_name.lastIndexOf('=') + 1));
+    let folder_name = url.substring(url.lastIndexOf('page=') + 5);
     console.log(folder_name);
-    console.log(typeof(folder_name));
+    folder_name = parseInt(folder_name);
+    if(isNaN(folder_name)) {
+        let body = document.body;
+        let h1 = document.createElement('h1');
+        h1.innerHTML = 'Please as the following example \n {url}?page={page_num}.';
+        body.appendChild(h1);
+        return;
+    }
+    console.log(folder_name);
     folder_name = folder_name%10;
     if (folder_name == 0) {
         folder_name = 10;
