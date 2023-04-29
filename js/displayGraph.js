99da4cec86a206748086c2c1d62565cf7e549a9f
jaya-shankar <bjayashankar2102@gmail.com>
2023-04-29 11:43:21 +0530

    "added graphes"

--- js/displayGraph.js
+++ js/displayGraph.js
@@ -1,4 +1,5 @@
 var file_names = ['1.png', '2.png', '3.png', '5.png', '6.png', '7.png', '8.png','9.png'];
+var csv_file_names = ['1.csv', '2.csv', '3.csv', '5.csv', '6.csv', '7.csv', '8.csv','9.csv'];
 document.addEventListener('DOMContentLoaded', function() {
     let url = window.location.href;
     let folder_name = url.substring(url.lastIndexOf('page=') + 5);
@@ -19,7 +20,7 @@ document.addEventListener('DOMContentLoaded', function() {
     for (var i = 0; i < file_names.length; i++) {
         var img = document.createElement("img");
         img.src = "graphs/"+folder_name+"/"+file_names[i];
-        // img.alt = "Graph";
+        img.alt = "graphs/"+folder_name+"/"+csv_file_names[i];
         img.className = 'centerImg'
         document.body.appendChild(img);
     }
