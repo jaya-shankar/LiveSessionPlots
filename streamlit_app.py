77e930083c70b67295bbb9a5d4a7ef53ddb6bc0e
jaya-shankar <bjayashankar2102@gmail.com>
2023-05-02 15:56:52 +0530

    add feat to select multiple countries

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,28 +7,25 @@ from utils import get_country_coords
 
 st.title('Plots')
 
+le_df = pd.read_csv("./edu_datasets/life_expectancy_years.csv")
+
+countries = le_df['Country'].unique()
 # Create a list of countries
-countries = ['South Korea','Argentina']
+# countries = ['South Korea','Argentina']
 indices = ['Primary Education', 'Lower Secondary Education', 'Higher Secondary Education', 'College Completion', 'GDP per Capita', 'Life Expectancy', 'Total Fertility Rate','Years']
 
 col1, col2 = st.columns(2)
 # Add a dropdown box to select a country
-selected_country_1 = col1.selectbox('Select a country 1', countries)
-selected_country_2 = col2.selectbox('Select a country 2', countries)
-
+selected_countries = st.multiselect('Select Countries', countries )
 selected_x = col1.selectbox('Select x axis', indices)
 selected_y = col2.selectbox('Select y axis', indices)
 
-
-
-
-country_1_coords = get_country_coords(selected_country_1, selected_x, selected_y)
-country_2_coords = get_country_coords(selected_country_2, selected_x, selected_y)
-
 # plot the line chart using Matplotlib
 fig, ax = plt.subplots()
-ax.plot(country_1_coords['x'], country_1_coords['y'], label=selected_country_1)
-ax.plot(country_2_coords['x'], country_2_coords['y'], label=selected_country_2)
+for selected_country in selected_countries:
+    country_coords = get_country_coords(selected_country, selected_x, selected_y)
+    ax.plot(country_coords['x'], country_coords['y'], label=selected_country)
+
 ax.set_xlabel(selected_x)
 ax.set_ylabel(selected_y)
 ax.set_title(f'{selected_x} vs {selected_y}')
@@ -36,3 +33,4 @@ ax.legend()
 
 # display the chart in Streamlit app
 st.pyplot(fig)
+st.write("**Note** : The graphs are plotted for data collected between the years 1960-2015")
