f360e8e88a80a0e866a5ff0d4e1b5fb7d4af2264
jaya-shankar <bjayashankar2102@gmail.com>
2023-05-06 14:07:27 +0530

    add downloadble option

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,36 +1,78 @@
+# pyright: reportMissingModuleSource=false
+# pyright: reportMissingImports=false
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
-from utils import get_country_coords
+from utils import (
+    get_country_coords,
+    save_csv,
+)
 
 # dropdown box for selecting country
 
-st.title('Plots')
+st.title("Plots 🌎")
 
 le_df = pd.read_csv("./edu_datasets/life_expectancy_years.csv")
 
-countries = le_df['Country'].unique()
+countries = le_df["Country"].unique()
 # Create a list of countries
 # countries = ['South Korea','Argentina']
-indices = ['Primary Education', 'Lower Secondary Education', 'Higher Secondary Education', 'College Completion', 'GDP per Capita', 'Life Expectancy', 'Total Fertility Rate','Years']
+indices = [
+    "Primary Education",
+    "Lower Secondary Education",
+    "Higher Secondary Education",
+    "College Completion",
+    "GDP per Capita",
+    "Life Expectancy",
+    "Total Fertility Rate",
+    "Years",
+]
 
 col1, col2 = st.columns(2)
 # Add a dropdown box to select a country
-selected_countries = st.multiselect('Select Countries', countries )
-selected_x = col1.selectbox('Select x axis', indices)
-selected_y = col2.selectbox('Select y axis', indices)
+selected_countries = st.multiselect("Select Countries", countries)
+selected_x = col1.selectbox("Select x axis", indices)
+selected_y = col2.selectbox("Select y axis", indices)
 
 # plot the line chart using Matplotlib
 fig, ax = plt.subplots()
+country_coords = None
 for selected_country in selected_countries:
     country_coords = get_country_coords(selected_country, selected_x, selected_y)
-    ax.plot(country_coords['x'], country_coords['y'], label=selected_country)
+    ax.plot(country_coords["x"], country_coords["y"], label=selected_country)
 
 ax.set_xlabel(selected_x)
 ax.set_ylabel(selected_y)
-ax.set_title(f'{selected_x} vs {selected_y}')
+ax.set_title(f"{selected_x} vs {selected_y}")
 ax.legend()
 
+if country_coords is not None:
+    col1, col2  = st.columns(2)
+    file_name   = f"{selected_x[:3]}_{selected_y[:3]}"
+
+    csv_snippet = save_csv(selected_countries, selected_x, selected_y)
+    with open("chart.csv", "rb") as f:
+        data_bytes = f.read()
+        col1.download_button(
+            label       = "Download Data 💿",
+            data        = data_bytes,
+            file_name   = f"{file_name}.csv",
+            mime        = "text/csv",
+        )
+
+    fig.savefig("chart.png")
+    with open("chart.png", "rb") as f:
+        image_bytes = f.read()
+        col2.download_button(
+            label       = "Download Graph 📈",
+            data        = image_bytes,
+            file_name   = f"{file_name}.png",
+            mime        = "image/png",
+        )
 # display the chart in Streamlit app
 st.pyplot(fig)
-st.write("**Note** : The graphs are plotted for data collected between the years 1960-2015")
+
+
+st.write(
+    "**Note** : The graphs are plotted for data collected between the years 1960-2015"
+)
--- utils.py
+++ utils.py
@@ -1,36 +1,57 @@
+# pyright: reportMissingModuleSource=false
 import pandas as pd
 
-root = "./edu_datasets/" 
+root = "./edu_datasets/"
 datasets_path = {
-                    "Primary Education"               :  root+ "20-24-Primary_fin.csv",
-                    "Lower Secondary Education"       :  root+ "20-24-Lower_Secondary_fin.csv",
-                    "Higher Secondary Education"      :  root+ "20-24-Higher_Secondary_fin.csv",
-                    "College Completion"              :  root+ "College_comp.csv",
-                    "Total Fertility Rate"            :  root+ "children_per_woman_total_fertility.csv",
-                    "GDP per Capita"                  :  root+ "gdppercapita_us_inflation_adjusted.csv",
-                    "Life Expectancy"                 :  root+ "life_expectancy_years.csv",
-                    "Years"                           :  root+ "years.csv",
-                    "india_tfr"                       :  root+ "India/TFR.csv"
-                    
-                }
+    "Primary Education": root + "20-24-Primary_fin.csv",
+    "Lower Secondary Education": root + "20-24-Lower_Secondary_fin.csv",
+    "Higher Secondary Education": root + "20-24-Higher_Secondary_fin.csv",
+    "College Completion": root + "College_comp.csv",
+    "Total Fertility Rate": root + "children_per_woman_total_fertility.csv",
+    "GDP per Capita": root + "gdppercapita_us_inflation_adjusted.csv",
+    "Life Expectancy": root + "life_expectancy_years.csv",
+    "Years": root + "years.csv",
+    "india_tfr": root + "India/TFR.csv",
+}
+
 
 def get_country_coords(country, x, y):
-    
     df_1 = pd.read_csv(datasets_path[x])
     df_2 = pd.read_csv(datasets_path[y])
-    
-    df_1 = df_1[df_1['Country'] == country]
-    df_2 = df_2[df_2['Country'] == country]
-    
-    df_1 = df_1.drop(['Country'], axis=1)
-    df_2 = df_2.drop(['Country'], axis=1)
-    
+
+    df_1 = df_1[df_1["Country"] == country]
+    df_2 = df_2[df_2["Country"] == country]
+
+    df_1 = df_1.drop(["Country"], axis=1)
+    df_2 = df_2.drop(["Country"], axis=1)
+
     df_1 = df_1.T
     df_2 = df_2.T
-    
+
     df_3 = df_1.merge(df_2, left_index=True, right_index=True)
-    df_3.columns = ['x', 'y']
+    df_3.columns = ["x", "y"]
     df_3.dropna(inplace=True)
     df_3.reset_index(drop=True, inplace=True)
-    
+
     return df_3
+
+
+def save_csv(selected_countries, x, y):
+    df_1 = pd.read_csv(datasets_path[x])
+    df_2 = pd.read_csv(datasets_path[y])
+    df_1 = df_1[df_1["Country"].isin(selected_countries)]
+    df_2 = df_2[df_2["Country"].isin(selected_countries)]
+
+    melted_df_1 = pd.melt(df_1, id_vars=["Country"], var_name="year", value_name=x)
+    print(melted_df_1)
+
+    melted_df_1["year"] = melted_df_1["year"].astype(int)
+
+    melted_df_2 = pd.melt(df_2, id_vars=["Country"], var_name="year", value_name=y)
+    melted_df_2["year"] = melted_df_2["year"].astype(int)
+
+    merged_df = pd.merge(melted_df_1, melted_df_2, on=["Country", "year"])
+    merged_df.sort_values(by=["Country", "year"], inplace=True)
+    
+    merged_df.to_csv("chart.csv", index=False)
+    return
